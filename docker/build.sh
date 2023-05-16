f507fd9892ee17041f911a1e5f9d0292de20f7f1
Salvador E. Tropea <salvador@inti.gob.ar>
2023-05-16 07:12:37 -0300

    Explicit dockerx

--- docker/build.sh
+++ docker/build.sh
@@ -3,5 +3,5 @@ mkdir -p apt_debs/archives/partial
 mkdir -p apt_files/lists/partial/
 mkdir -p apt_files/lists/auxfiles/
 TAG=sd1.2.1_pt1.13.1_rocm5.2_d11.7
-docker build -f Dockerfile --progress plain -t setsoft/sd_webui:${TAG} . 2>&1 | tee build.log
-docker build -f Dockerfile --progress plain -t setsoft/sd_webui:latest . 2>&1 | tee build.log
+docker buildx build -f Dockerfile --progress plain -t setsoft/sd_webui:${TAG} . 2>&1 | tee build.log
+docker buildx build -f Dockerfile --progress plain -t setsoft/sd_webui:latest . 2>&1 | tee build.log
