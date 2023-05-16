a2643e53fa0271d2528e73116a5cc7bd3e92070a
Salvador E. Tropea <salvador@inti.gob.ar>
2023-05-16 07:00:13 -0300

    Added oush to build

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,8 @@
 mkdir -p apt_debs/archives/partial
 mkdir -p apt_files/lists/partial/
 mkdir -p apt_files/lists/auxfiles/
-docker build -f Dockerfile --progress plain -t setsoft/sd_webui:sd1.2.1_pt1.13.1_rocm5.2_d11.7 . 2>&1 | tee build.log
+TAG=sd1.2.1_pt1.13.1_rocm5.2_d11.7
+docker build -f Dockerfile --progress plain -t setsoft/sd_webui:${TAG} . 2>&1 | tee build.log
 docker build -f Dockerfile --progress plain -t setsoft/sd_webui:latest . 2>&1 | tee build.log
+docker push setsoft/sd_webui:${TAG}
+docker push setsoft/sd_webui:latest
