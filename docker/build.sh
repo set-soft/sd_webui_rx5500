f7bb03aa931b10acdb019d129d9949a000bfb7f8
Salvador E. Tropea <salvador@inti.gob.ar>
2023-05-19 22:16:11 -0300

    Changed the tag

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,6 @@
 mkdir -p apt_debs/archives/partial
 mkdir -p apt_files/lists/partial/
 mkdir -p apt_files/lists/auxfiles/
-TAG=sd1.2.1_pt1.13.1_rocm5.2_d11.7
+TAG=sd1.2.1_cn1.1_pt1.13.1_rocm5.2_d11.7
 docker buildx build -f Dockerfile --progress plain -t setsoft/sd_webui:${TAG} . 2>&1 | tee build.log
 docker buildx build -f Dockerfile --progress plain -t setsoft/sd_webui:latest . 2>&1 | tee build.log
